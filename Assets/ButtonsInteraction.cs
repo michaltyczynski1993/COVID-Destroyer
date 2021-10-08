e2c2960302e587c2fa81b7f18dc69198d0e4b33e
Michał <m.tyczynski1993@gmail.com>
2021-10-08 07:57:35 +0200

    Basic prototype for menu with Gaze UI

---
+++ Assets/ButtonsInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsInteraction : MonoBehaviour
+{
+   public void Enter()
+   {
+       SceneManager.LoadScene(1);
+   }
+}
